ba8ccea278488de19caefc492440bc00301bddc1
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-17 00:37:53 +0600

    typeorm & @nestjs/typeorm

---
+++ db.sql
@@ -0,0 +1,5 @@
+create database mediumclone;
+
+create user mediumclone with encrypted password '';
+
+grant all privileges on database mediumclone to mediumclone;
---
+++ src/ormconfig.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { ConnectionOptions } from 'typeorm';
+dotenv.config();
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: process.env.PASSWORD,
+  database: 'mediumclone',
+};
+
+export default config;
